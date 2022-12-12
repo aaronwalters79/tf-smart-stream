b9dcf70d6eeb4bd361842ffb2da301f9ce60aba1
Walters <35389994+aaronwalters79@users.noreply.github.com>
2022-12-12 11:52:49 -0500

    Added Production configurations.

---
+++ tf-smart-stream-binary-prod/main.tf
@@ -0,0 +1,33 @@
+#resource "google_pubsub_topic" "example" {
+#  project = "topic-project"
+#  name    = "example-topic"
+#}
+
+#export GOOGLE_IMPERSONATE_SERVICE_ACCOUNT=smart-stream-subscriber-servic@YOUR-PROJECT-NAME.iam.gserviceaccount.com
+
+
+locals {
+  customer_project = "cme-markets-analysis"  #YOUR PROJECT NAME
+  cme_topic_project = "cmegroup-marketdata" #CME PRODEnvironment for Binary FIX Feeds
+
+}
+
+
+
+
+#Example of single stream creation
+#resource "google_pubsub_subscription" "CERT-SSCL-GCP-MD-RT-CMEG-FIXBIN-v01000-INCR-310" {
+#  project = local.customer_project
+#  name    = "PROD.SSCL.GCP.MD.RT.CMEG.STRBIN.v01000.INCR.200"
+#  topic   = "projects/${local.cme_topic_project}/topics/PROD.SSCL.GCP.MD.RT.CMEG.STRBIN.v01000.INCR.200"
+#}
+
+
+#example of multiple stream creation using configuration tf file as iterator
+
+resource "google_pubsub_subscription" "ALL-CERT-BINARY" {
+    project = local.customer_project
+    for_each = var.prod_topics
+    name = each.value
+    topic = "projects/${local.cme_topic_project}/topics/${each.value}"
+}
