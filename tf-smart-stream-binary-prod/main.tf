79314572e144bd283e5b7ea15ad4395784a73874
Walters <35389994+aaronwalters79@users.noreply.github.com>
2022-12-12 20:06:51 -0500

    Update main.tf
    correct the names from cert to prod...

--- tf-smart-stream-binary-prod/main.tf
+++ tf-smart-stream-binary-prod/main.tf
@@ -7,7 +7,7 @@
 
 
 locals {
-  customer_project = "cme-markets-analysis"  #YOUR PROJECT NAME
+  customer_project = "YOUR PROJECT NAME"  #YOUR PROJECT NAME
   cme_topic_project = "cmegroup-marketdata" #CME PRODEnvironment for Binary FIX Feeds
 
 }
@@ -16,16 +16,17 @@ locals {
 
 
 #Example of single stream creation
-#resource "google_pubsub_subscription" "CERT-SSCL-GCP-MD-RT-CMEG-FIXBIN-v01000-INCR-310" {
+#resource "google_pubsub_subscription" "PROD.SSCL.GCP.MD.RT.CMEG.STRBIN.v01000.INCR.200" {
 #  project = local.customer_project
 #  name    = "PROD.SSCL.GCP.MD.RT.CMEG.STRBIN.v01000.INCR.200"
 #  topic   = "projects/${local.cme_topic_project}/topics/PROD.SSCL.GCP.MD.RT.CMEG.STRBIN.v01000.INCR.200"
 #}
 
 
+
 #example of multiple stream creation using configuration tf file as iterator
 
-resource "google_pubsub_subscription" "ALL-CERT-BINARY" {
+resource "google_pubsub_subscription" "ALL-PROD-BINARY" {
     project = local.customer_project
     for_each = var.prod_topics
     name = each.value
